48057c7f98e03fcfdcc821f0529f1ad846e1d575
Andrew Miller <206alm@gmail.com>
2020-01-12 07:11:53 -0800

    basic ui built, working on mongo now'

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
-const express = require('express');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 8080;
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+const axios = require("axios");
+const cheerio = require("cheerio");
 
+// Require all models
+const db = require("./models");
+
+const PORT = process.env.PORT || 3000;
+
+// Initialize Express
 const app = express();
 
-app.use(express.static('public'));
+// Configure middleware
 
-app.use(express.urlencoded({ extended:true }));
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
 const exphbs = require('express-handlebars');
 
